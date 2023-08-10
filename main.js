7f50d88924e70524d1a11f579269f1fea4c4c2b2
peihsuan <june625678@gmail.com>
2023-08-10 17:26:06 +0800

    version 1

---
+++ main.js
@@ -0,0 +1,18 @@
+import "./assets/scss/all.scss";
+import "bootstrap/dist/js/bootstrap.min.js";
+
+window.addEventListener("load", () => {
+  let adminPage = document.querySelector("#adminPage");
+  let assignmentPage = document.querySelector("#assignmentPage");
+  if (
+    window.location.pathname == "/system-interface/pages/index.html"
+  ) {
+    adminPage.classList.add("active");
+  }
+  if (
+    window.location.pathname ==
+    "/system-interface/pages/assignment.html"
+  ) {
+    assignmentPage.classList.add("active");
+  }
+});
